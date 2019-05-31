7873163add1a62db2712967afa3579144c73afa3
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2019-06-01 01:27:28 +0530

    refactoring code structure

--- genman.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pandoc -s -f markdown -t man README >fdkaac.1 && mv -f fdkaac.1 man/fdkaac.1
--- version.h
+++
@@ -1,4 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-const char *fdkaac_version = "1.0.0";
-#endif
